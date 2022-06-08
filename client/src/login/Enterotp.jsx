ae4726c8d5c27143d408b5528b3c7790cac3c485
Shriya003 <sriyasri003@gmail.com>
2022-06-08 09:36:41 +0530

    Css changed

--- client/src/login/Enterotp.jsx
+++ client/src/login/Enterotp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom';
+require("../style/Enterotp.css")
 
 function Enterotp() {
 
@@ -31,14 +32,24 @@ function Enterotp() {
 
     return (
         <>
-        <h1>helllo</h1>
         <div className="otpdiv">
+        <div className='main_box'>
+            
+         <div className="box">
+             <h1 className='otp_h'>Enter Otp</h1>
+             <div className='otp_field'>
+                 <input type="text" maxLength={1}/>
+                 <input type="text" maxLength={1}/>
+                 <input type="text" maxLength={1}/>
+                 <input type="text" maxLength={1}/>
+             </div>
+             
+        {/* <input type="password" name="password" id="otp" placeholder="otp" value={otp} onChange={(e)=>{setotp(e.target.value)}} />
 
-        <input type="password" name="password" id="otp" placeholder="otp" value={otp} onChange={(e)=>{setotp(e.target.value)}} />
+         <button onClick={validate_otp}> send otp </button> */}
 
-         <button onClick={validate_otp}> send otp </button>
-
-
+         </div>
+             </div>
 
         </div>
 
